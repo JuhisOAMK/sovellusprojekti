58fc9a010e50854ad0519115e6465034fcf341ae
JuhisOAMK <57358969+JuhisOAMK@users.noreply.github.com>
2019-11-16 17:31:19 +0200

    Update sketch_nov14a.ino
    0.2

--- sketch_nov14a.ino
+++ sketch_nov14a.ino
@@ -10,15 +10,32 @@
 #define oikea 6
 #define ylos 3
 #define alas 2
+#define eimitaan 0
 
 #define screenWidth 160
-#define screenHeigth 128
+#define screenHeight 128
 #define tileSize 6
 
-int neliox = 0;
-int nelioy = 0;
+int force = 5, gravity = 0.5;
+
+
+const int anag0 = A0;
+const int anog1 = A1;
+const int anog2 = A2;
+const int anog3 = A3;
+int inputA = 0;
+int inputB = 0;
+int inputC = 0;
+int inputD = 0;
+
+
+int neliox = 70;
+int nelioy = 90;
 int oldneliox = 0;
 int oldnelioy = 0;
+int nelionopeus = 1;
+int nelionsuunx = 1;
+int nelionsuuny = 1;
 
 
 
@@ -27,18 +44,100 @@ TFT screen = TFT(LCD_CS, DC, RESET);
 
 void setup() {
   screen.begin();
+  Serial.begin(9600);
   screen.background(255, 255, 255);
+  screen.stroke(255, 255, 255);
 
 }
 
 void loop() {
 
-  int myWidth = TFTscreen.width();
-  int myHeight = TFTscreen.height();
+inputA = map(analogRead(anag0), 0, 1023, 0, 255);
+inputB = map(analogRead(anog1), 0, 1023, 0, 255);
+inputC = map(analogRead(anog2), 0, 1023, 0, 255);
+inputD = map(analogRead(anog3), 0, 1023, 0, 255);
+Serial.print(inputA);
+Serial.print("       ");
+Serial.print(inputB);
+Serial.print("       ");
+Serial.print(inputC);
+Serial.print("       ");
+Serial.print(inputD);
+Serial.print("\n");
 
+
+int scwidth = screen.width();
+int scheight = screen.height();
+screen.fill(0,0,0);
+if (millis() % nelionopeus < 2){
+  liiku();
+}
+
+  
 }
 
 
 void liiku() {
+  int A = 0;
+  int B = 0;
+  int C = 0;
+  int D = 0;
+  A = map(analogRead(anag0), 0, 100, 0, 255);
+  B = map(analogRead(anog1), 0, 100, 0, 255);
+  C = map(analogRead(anog2), 0, 100, 0, 255);
+  D = map(analogRead(anog3), 0, 100, 0, 255);
+  if (neliox > screen.width() || neliox < 0){
+    nelionsuunx = -nelionsuunx;
+  }
+
+  if (nelioy > screen.height() || nelioy < 0){
+    nelionsuuny = -nelionsuuny;
+  }
+
+  if (A > 2500)
+  {
+    neliox += nelionsuunx;
+    Serial.print(A);
+    Serial.print(" ");
+  }
+
+  if (B > 2500)
+  {
+    nelioy -= nelionsuuny;
+  }
+
+  if (C > 2500)
+  {
+    neliox -= nelionsuunx;
+  }
+
+  if (D > 2500)
+  {
+    nelioy -= nelionsuuny + force;
+    //nelioy -= nelionsuuny;
+  }
+
+  if (nelioy > )
+  {
+    nelioy += nelionsuuny - gravity;
+  }
+
+  /*
+  neliox += nelionsuunx;
+  nelioy += nelionsuuny;
+  */
+
+  screen.fill(0, 0, 0);
+
+  if (oldneliox != neliox || oldnelioy != nelioy) {
+    screen.rect(oldneliox, oldnelioy, 5, 5);
+    }
+
+  screen.fill(0, 0, 0);
+  screen.rect(neliox, nelioy, 5, 5);
+
+  oldneliox = neliox;
+  oldnelioy = nelioy;
+  
   
 }
